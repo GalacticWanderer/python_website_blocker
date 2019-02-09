5a56f2507e9f06c426b194ad2c2ebddc3525bf21
Joy Paul <jpaul3250@gmail.com>
2019-02-09 01:17:37 -0500

    appending and removal of sites is now successful

--- site_blocker.py
+++ site_blocker.py
@@ -1,4 +1,5 @@
 import time  # for sleep functionality
+import re
 from datetime import datetime as dt  # for system date and time
 
 # vars to hold host file address, redirect ip and a list of sites to block
@@ -9,7 +10,7 @@ website_list = ["www.reddit.com", "reddit.com", "apple.com", "yahoo.com"]
 
 # using the datetime func, storing the shift start and end time
 work_start = dt(dt.now().year, dt.now().month, dt.now().day, 8)
-work_end = dt(dt.now().year, dt.now().month, dt.now().day, 22)
+work_end = dt(dt.now().year, dt.now().month, dt.now().day, 16)
 
 #  the while func makes sure the program runs indefinitely
 while True:
@@ -27,11 +28,26 @@ while True:
                     pass
                 elif site not in contents:
                     print(site + " not here, appending it!")
+                    # appending new items on the file
                     file.write(redirect + " " + site+"\n")
 
     else:
         print("non working hours")
-        #  remove the site lists and the redirect address
-
-    #  sleeps for 5 secs before continuing the while loop
+        #  opening the file
+        with open(tempHostAddress, 'r+') as file:
+            # reading the file as individual lines instead
+            contents = file.readlines()
+            # moving the pointer to the beginning to the line so we can append
+            # to it at the beginning
+            file.seek(0)
+            # for loop for every line in content
+            for line in contents:
+                # if not any of the websites from website_list is in line,
+                # then write the lines at the "seek point"
+                if not any(website in line for website in website_list):
+                    print("replacing sites")
+                    file.write(line)
+            # truncates remove the texts from before
+            file.truncate()
+        #  sleeps for 5 secs before continuing the while loop
     time.sleep(5)
